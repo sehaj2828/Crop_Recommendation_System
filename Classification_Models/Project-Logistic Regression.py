1b8bb7f9c2d48a9c9bd4b45a48d781f79ab29303
Sehajveer Singh <sehaj2828@gmail.com>
2022-07-30 21:13:54 +0530

    Classification Models

---
+++ Classification_Models/Project-Logistic Regression.py
@@ -0,0 +1,44 @@
+# Logistic Regression
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Project-CropDataset.csv")
+X = dataset.iloc[:, 0:-1 ].values
+y = dataset.iloc[:, -1 ].values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+
+from sklearn.metrics import plot_confusion_matrix
+plot_confusion_matrix(classifier, X_test, y_test)  
+plt.show()
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+from sklearn.metrics import accuracy_score
+Acc_score = accuracy_score(y_test, y_pred)
+print(Acc_score)
+
+from sklearn.metrics import recall_score
+Rscore = recall_score(y_test, y_pred, average='macro')
+print(Rscore)
+
+from sklearn.metrics import precision_score
+Pscore = precision_score(y_test, y_pred, average='macro')
+print(Pscore)
+
+from sklearn.metrics import f1_score
+Fscore = f1_score(y_test, y_pred, average='macro')
+print(Fscore)
+
---
+++ Classification_Models/Project-XG Boost.py
@@ -0,0 +1,36 @@
+# XG Boost
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Project-CropDataset.csv')
+X = dataset.iloc[:, 0:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Training XGBoost on the Training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import plot_confusion_matrix
+plot_confusion_matrix(classifier, X_test, y_test)  
+plt.show()
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+
+print(cm)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
